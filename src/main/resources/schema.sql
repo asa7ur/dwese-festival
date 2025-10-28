439559419aed1bf11f8b8b4a2b893fad43d81dd1
Daniel-goga-GIT <danielgongar8@gmail.com>
2025-10-28 15:42:26 +0100

    Inserccion de data y schema de Stages y Spectators

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,21 +1,21 @@
-CREATE TABLE artistas (
-    id INT PRIMARY KEY,
-    nombre VARCHAR(100),
-    pais VARCHAR(50),
-    escenario_id INT
+CREATE DATABASE IF NOT EXISTS FestivalDB;
+USE FestivalDB;
+
+CREATE TABLE IF NOT EXISTS Stages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    code VARCHAR(20) UNIQUE,
+    name VARCHAR(100),
+    capacity INT
 );
 
-CREATE TABLE tickets (
-    id INT PRIMARY KEY,
-    precio DECIMAL(8,2),
-    id_espectador INT,
-    FOREIGN KEY (id_espectador) REFERENCES espectador(id)
+CREATE TABLE IF NOT EXISTS Spectators (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    code VARCHAR(20) UNIQUE,
+    name VARCHAR(50),
+    last_name VARCHAR(100),
+    birth_date DATE,
+    phone VARCHAR(20),
+    email VARCHAR(100),
+    city VARCHAR(100),
+    country VARCHAR(50)
 );
-
-CREATE TABLE escenarios (
-    id INT PRIMARY KEY,
-    nombre VARCHAR(100),
-    capacidad INT
-);
-
-
