753429505cc08249698f185ee4569d1f8591e533
Garik Asatryan <asa7ur@proton.me>
2025-11-03 15:53:01 +0100

    Corregido el tipo de datos de startTime y endTime

--- src/main/java/org/iesalixar/daw2/danielgarik/dwese_festival/entities/Performance.java
+++ src/main/java/org/iesalixar/daw2/danielgarik/dwese_festival/entities/Performance.java
@@ -3,9 +3,11 @@ package org.iesalixar.daw2.danielgarik.dwese_festival.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.sql.Date;
 import java.sql.Time;
+import java.time.LocalTime;
 
 @Data
 @AllArgsConstructor
@@ -14,14 +16,16 @@ public class Performance {
     private Long id;
     private String code;
     private Date date;
-    private Time startTime;
-    private Time endTime;
+    @DateTimeFormat(pattern = "HH:mm")
+    private LocalTime startTime;
+    @DateTimeFormat(pattern = "HH:mm")
+    private LocalTime endTime;
     private Long artistId;
     private String artistName;
     private Long stageId;
     private String stageName;
 
-    public Performance(String code, Date date, Time startTime, Time endTime, Long artistId, Long stageId) {
+    public Performance(String code, Date date, LocalTime startTime, LocalTime endTime, Long artistId, Long stageId) {
         this.code = code;
         this.date = date;
         this.startTime = startTime;
